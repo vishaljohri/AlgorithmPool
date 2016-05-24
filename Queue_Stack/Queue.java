645878de047da170dd6130d208a3a2ec13228f4b
Vishal Johri <vishaljohri@yahoo.co.in>
2016-05-24 13:17:55 -0700

    Queue operations

---
+++ Queue_Stack/Queue.java
@@ -0,0 +1,84 @@
+
+public class Queue {
+	int front;
+	int rear;
+	int []queue;
+	int maxSize;
+	int count;
+	
+	Queue(int maxSize) {
+		this.maxSize = maxSize;
+		queue = new int[this.maxSize];
+		front = -1;
+		rear = -1;
+	}
+	
+	void enqueue(int n) {
+		if(((rear+1)%maxSize) == front) {
+			System.out.println("queue overflow");
+			return;
+		}
+		int newRear = (rear+1)%maxSize;
+		rear = newRear;
+		if(front == -1) {
+			front++;
+			queue[rear] = n;
+			count++;
+			return;
+		}
+		queue[rear] = n;
+		count++;
+	}
+	
+	int dequeue() {
+		if(front == -1) {
+			System.out.println("queue underflow");
+			return 0;
+		}
+		count--;
+		return(queue[(front++)%maxSize]);
+		
+	}
+	
+	void reverseQueueFirstKElements(int k) {
+		java.util.Stack<Integer> tempStack = new java.util.Stack<>();
+		for(int i = 1; i <= k; i++) {
+			tempStack.push(this.dequeue());
+		}
+		while(!tempStack.empty()) {
+			this.enqueue(tempStack.pop());
+		}
+		
+		for(int i = 1; i <= count-k; i++) {
+			int x = this.dequeue();
+			this.enqueue(x);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Queue q = new Queue(5);
+		q.enqueue(4);
+		q.enqueue(5);
+		q.enqueue(6);
+		q.enqueue(7);
+		q.enqueue(8);
+		q.enqueue(9);
+		System.out.println(q.dequeue());
+		q.enqueue(9);
+		q.enqueue(1);
+		q.enqueue(14);
+		q.enqueue(6);
+		q.enqueue(2);
+		System.out.println(q.dequeue());
+		
+		Queue q2 = new Queue(5);
+		q2.enqueue(4);
+		q2.enqueue(5);
+		q2.enqueue(6);
+		q2.enqueue(7);
+		q2.reverseQueueFirstKElements(2);
+
+	}
+
+}
