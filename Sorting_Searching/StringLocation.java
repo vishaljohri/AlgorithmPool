87b6e89311e373b0251946274562903d9dfe508c
Vishal Johri <vishaljohri@yahoo.co.in>
2016-06-06 13:29:33 -0700

    Search for string location in sequence of ordered strings having blank strings as well.

---
+++ Sorting_Searching/StringLocation.java
@@ -0,0 +1,56 @@
+
+public class StringLocation {
+
+	int location(String str[], String key) {
+		for (int i = 0; i < str.length; i++) {
+			if (key.equals(str[i]))
+				return i;
+		}
+		return -1;
+	}
+
+	int locationBinary(String str[], int start, int end, String key) {
+		if (start > end)
+			return -1;
+		int mid = (start + end) / 2;
+		if (str[mid].equals("")) {
+			int left = mid - 1;
+			int right = mid + 1;
+			while (true) {
+				if (left < start && right > end)
+					return -1;
+				if (left >= start && str[left].equals("")) {
+					left--;
+				} else {
+					mid = left;
+					break;
+				}
+				if (right <= end && str[right].equals("")) {
+					right++;
+				} else {
+					mid = right;
+					break;
+				}
+			}
+		}
+		
+		if (str[mid].equals(key))
+			return mid;
+		if (str[mid].compareTo(key) > 0) {
+			return locationBinary(str, start, mid - 1, key);
+		} else
+			return locationBinary(str, mid + 1, end, key);
+
+	}
+
+	public static void main(String[] args) {
+		StringLocation sl = new StringLocation();
+		String str[] = { "at", "", "", "", "ball", "", "", "car", "", "",
+				"dad", "", "" };
+		System.out.println("location: " + sl.location(str, "ba"));
+		System.out.println("location using modified binary search: "
+				+ sl.locationBinary(str, 0, str.length - 1, "ball"));
+
+	}
+
+}
