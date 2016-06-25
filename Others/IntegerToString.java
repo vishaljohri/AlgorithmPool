616a54c2f05ea60ec2c6a00a19d4ced5bef52c69
Vishal Johri <vishaljohri@yahoo.co.in>
2016-06-25 15:39:10 -0700

    Convert given integer to string.

---
+++ Others/IntegerToString.java
@@ -0,0 +1,28 @@
+package programming.interviews.exposed.book;
+
+public class IntegerToString {
+	
+	String convert(int number) {
+		boolean isNegative = false;
+		if(number < 0) {
+			isNegative = true;
+			number = -number;
+		}
+		String str = "";
+		while(number != 0) {
+			str = number % 10 + str;
+			number = number / 10;
+		}
+		if(isNegative)
+			str = "-" + str;
+		return str;
+	}
+
+	public static void main(String[] args) {
+		int number = 14;
+		IntegerToString is = new IntegerToString();
+		System.out.println(is.convert(number));
+
+	}
+
+}
